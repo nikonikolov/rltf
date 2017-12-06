a54acb4118e065e99a9416d732fa31566ddbb0bf
nikonikolov <niko.b.nikolov@gmail.com>
2017-12-06 21:22:49 +0100

    reward scale wrapper and update target

---
+++ rltf/env_wrappers/wrappers.py
@@ -0,0 +1,113 @@
+import cv2
+import gym
+import numpy as np
+
+class ScaleReward(gym.RewardWrapper):
+  """Scale rewards"""
+
+  def __init__(self, env, scale):
+    super().__init__(env)
+    self.scale = scale
+
+  def _reward(self, reward):
+    return self.scale * reward
+
+
+# class RepeatAndStackLowDim(gym.Wrapper):
+#   """Repeat the action n times and stack the observations"""
+
+#   def __init__(self, env, n):
+#     """
+#     Args:
+#       n: int. Number of steps to repeat and stack
+#     """
+#     super().__init__(env)
+
+#     obs_shape = list(env.observation_space.shape)
+
+#     obs_shape = [n] + obs_shape
+#     dtype = np.float32
+#     low   = np.tile(env.observation_space.low[None:],   [n, 1])
+#     high  = np.tile(env.observation_space.high[None:],  [n, 1])
+
+#     self._obs_buf = np.zeros(obs_shape, dtype=nps.float32)
+#     self._n       = n
+#     self.observation_space = gym.spaces.Box(low=low, high=high, shape=obs_shape)
+
+
+#   def _step(self, action):
+#     """Repeat action, sum reward, and stack observations. Note that if
+#     episode terminates in the middle, the last observation is duplicated"""
+
+#     total_reward = 0.0
+#     done = False
+#     for i in range(self._n):
+#       if not done:
+#         obs, reward, done, info = self.env.step(action)
+#         total_reward += reward
+#       self._obs_buf[i, :] = obs
+
+#     return self._obs_buf, total_reward, done, info
+
+
+# class RepeatAndStackImage(gym.Wrapper):
+#   """Repeat the action n times and stack the observations"""
+
+#   def __init__(self, env, n):
+#     """
+#     Args:
+#       n: int. Number of steps to repeat and stack
+#     """
+#     super().__init__(env)
+
+#     obs_shape = list(env.observation_space.shape)
+
+#     self._nchannels = obs_shape[-1]
+
+#     obs_shape[-1]   = n * self._nchannels
+
+#     self._obs_buf = np.zeros(obs_shape, dtype=np.uint8)
+#     self._n       = n
+#     self.observation_space = gym.spaces.Box(low=0, high=255, shape=obs_shape)
+
+
+#   def _step(self, action):
+#     """Repeat action, sum reward, and stack observations. Note that if
+#     episode terminates in the middle, the last observation is duplicated"""
+
+#     total_reward = 0.0
+#     done = False
+#     for i in range(self._n):
+#       if not done:
+#         obs, reward, done, info = self.env.step(action)
+#         total_reward += reward
+#       self._obs_buf[:, i*self._nchannels] = obs
+
+#     return self._obs_buf, total_reward, done, info
+
+
+
+# class ResizeFrame(gym.ObservationWrapper):
+#   def __init__(self, env, width, height):
+#     """Warp frames to 84x84 as done in the Nature paper and later work."""
+#     super().__init__(env)
+#     self.width = width
+#     self.height = height
+#     self.observation_space = gym.spaces.Box(low=0, high=255, shape=(self.height, self.width, 1))
+
+#   def _observation(self, frame):
+#     # COLOR_RGB2GRAY is eqivalent to Y channel
+#     # See CV docs at https://docs.opencv.org/3.1.0/de/d25/imgproc_color_conversions.html
+#     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+#     frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+#     return frame[:, :, None]
+
+# def wrap_ddpg_image(env):
+#   env = ResizeFrame(env)
+#   env = RepeatAndStackImage(env)
+
+
+# def wrap_ddpg_low_dim(env):
+#   env = RepeatAndStackLowDim(env)
+
+#   return env
