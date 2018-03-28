c6529f57bfac5b49514fc2c8891ee8f0a7e1339e
nikonikolov <niko.b.nikolov@gmail.com>
2018-03-28 15:35:03 +0200

    Add Woodburry inverse and speed up BLR

--- rltf/models/blr.py
+++ rltf/models/blr.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
-from rltf.models.tf_utils import cholesky_inverse
-from rltf.models.tf_utils import sherman_morrison_inverse
+from rltf.models.tf_utils import woodburry_inverse
 
 
 class BayesianLinearRegression:
@@ -42,21 +41,17 @@ class BayesianLinearRegression:
     self.w_Lambda = tf.Variable(1.0/(self.tau**2) * I,      trainable=False)
 
 
-  def weight_posterior(self, X, y, sequential=False):
+  def weight_posterior(self, X, y):
     """Compute the weight posteriror of Bayesian Linear Regression
     Args:
       X: tf.Tensor, `shape=[None, D]`. The feature matrix
       y: tf.Tensor, `shape=[None, 1]`. The correct outputs
-      sequential: bool. If True, updates are performed sequentially with one data point
-        at a time. If False, update is done for the whole batch at once
     Returns:
       tf.Op which performs the update operation
     """
     X = self._add_bias(X)
 
-    if sequential:
-      return self._sequential_weight_posterior(X, y)
-    return self._batch_weight_posterior(X, y)
+    return self._weight_posterior(X, y)
 
 
   def _add_bias(self, x):
@@ -82,7 +77,7 @@ class BayesianLinearRegression:
     return tf.group(mu_op, Sigma_op, Lambda_op)
 
 
-  def _batch_weight_posterior(self, X, y):
+  def _weight_posterior(self, X, y):
     """Compute the weight posteriror of Bayesian Linear Regression
     Args:
       X: tf.Tensor, `shape=[None, D]`. The feature matrix
@@ -93,40 +88,11 @@ class BayesianLinearRegression:
     # Compute the posterior precision matrix
     w_Lambda = self.w_Lambda + self.beta * tf.matmul(X, X, transpose_a=True)
 
+    X_scaled = 1.0/self.sigma * X
+
     # Compute the posterior covariance matrix
-    # w_Sigma = cholesky_inverse(w_Lambda)
-    w_Sigma = tf.matrix_inverse(w_Lambda)
-
-    # Compute the posterior mean
-    w_mu = self._posterior_mean(X, y, self.w_mu, self.w_Lambda, w_Sigma)
-
-    return self._tf_update_params(w_mu, w_Sigma, w_Lambda)
-
-
-  def _sequential_weight_posterior(self, X, y):
-    """Update the weight posterior point by point
-    Args:
-      X: tf.Tensor, `shape=[None, D]`. The feature matrix
-      y: tf.Tensor, `shape=[None, 1]`. The correct outputs
-    Returns:
-      tf.Op which performs the update operation
-    """
-    N = tf.shape(X)[0]
-    i = tf.constant(0)
-    w_Sigma   = tf.identity(self.w_Sigma)
-    w_Lambda  = tf.identity(self.w_Lambda)
-
-    def body(i, w_Sigma, w_Lambda):
-      x = tf.expand_dims(X[i], axis=0)
-      w_Sigma, w_Lambda = self._posterior_cov_single_point(x, w_Sigma, w_Lambda)
-      return i+1, w_Sigma, w_Lambda
-
-    def cond(i, w_Sigma, w_Lambda):
-      return tf.less(i, N)
-
-    # Compute the posterior precision and covariance
-    out = tf.while_loop(cond, body, [i, w_Sigma, w_Lambda], parallel_iterations=1, back_prop=False)
-    _, w_Sigma, w_Lambda = out
+    w_Sigma = woodburry_inverse(self.w_Sigma, tf.transpose(X_scaled), X_scaled)
+    # w_Sigma = tf.matrix_inverse(w_Lambda)
 
     # Compute the posterior mean
     w_mu = self._posterior_mean(X, y, self.w_mu, self.w_Lambda, w_Sigma)
@@ -147,30 +113,6 @@ class BayesianLinearRegression:
     w_mu = tf.matmul(w_Sigma, self.beta * tf.matmul(X, y, True) + tf.matmul(w_prior_Lambda, w_prior_mu))
     w_mu = tf.cond(tf.reduce_all(tf.equal(X, tf.zeros_like(X))), lambda: w_prior_mu, lambda: w_mu)
     return w_mu
-
-
-  def _posterior_cov_single_point(self, x, w_prior_Sigma, w_prior_Lambda):
-    """Compute the weight posteriror covariance and precision of
-    Bayesian Linear Regression with only one new training data point
-    Args:
-      x: tf.Tensor, `shape=[1, D]`. The feature vector
-      w_prior_Sigma: tf.Tensor, `shape=[D, D]`. The covariance of the Gaussian prior
-      w_prior_Lambda: tf.Tensor, `shape=[D, D]`. The precision of the Gaussian prior
-    Returns:
-      w_Sigma: tf.Tensor, `shape=[D, D]`. The covariance of the Gaussian posterior
-      w_Lambda: tf.Tensor, `shape=[D, D]`. The precision of the Gaussian posterior
-    """
-
-    x_T = tf.transpose(x)
-
-    # Compute the posterior precision matrix
-    w_Lambda = w_prior_Lambda + self.beta * tf.matmul(x_T, x)
-
-    # Compute the posterior covariance matrix
-    u = 1.0 / self.sigma * x_T
-    w_Sigma = sherman_morrison_inverse(w_prior_Sigma, u, u)
-
-    return w_Sigma, w_Lambda
 
 
   def predict(self, X):
