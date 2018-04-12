7e36610b3dd3b5eda92193d5c3a4b22ecaea7e17
nikonikolov <niko.b.nikolov@gmail.com>
2018-04-12 16:13:35 +0200

    exponential decay schedule

--- rltf/schedules/__init__.py
+++ rltf/schedules/__init__.py
@@ -1,4 +1,5 @@
 from rltf.schedules.schedule            import Schedule
 from rltf.schedules.const_schedule      import ConstSchedule
+from rltf.schedules.exponential_decay   import ExponentialDecay
 from rltf.schedules.linear_schedule     import LinearSchedule
 from rltf.schedules.piecewise_schedule  import PiecewiseSchedule
