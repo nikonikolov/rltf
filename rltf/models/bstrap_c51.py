fix rho normalization

rltf/models/bstrap_c51.py
@@ -221,9 +221,12 @@ class BaseBstrapC51(BaseBstrapDQN):
     z_var   = tf.reduce_sum(z_var, axis=1) / float(self.n_heads-1)  # out: [None, n_actions]
 
     # Normalize the variance
-    a_var   = tf.reduce_sum(tf.square(z_var), axis=-1)    # out: [None]
-    a_var   = tf.expand_dims(tf.sqrt(a_var), axis=-1)     # out: [None, 1]
-    z_var   = z_var / a_var                               # out: [None, n_actions]
+    # a_var   = tf.reduce_mean(tf.square(z_var), axis=-1)   # out: [None]
+    # a_var   = tf.expand_dims(tf.sqrt(a_var), axis=-1)     # out: [None, 1]
+    # z_var   = z_var / a_var                               # out: [None, n_actions]
+    mean    = tf.reduce_mean(z_var, axis=-1, keep_dims=True)  # out: [None, 1]
+    z_var   = z_var / mean                                    # out: [None, n_actions]
+
     return z_var
 
 

rltf/models/bstrap_dqnc51.py
@@ -189,15 +189,17 @@ class BaseBstrapDQNC51(BaseBstrapDQN):
     z       = agent_net[1]
 
     # Var(X) = sum_x p(X)*[X - E[X]]^2
-    q       = tf.reduce_sum(z * self.bins, axis=-1)
+    q       = tf.reduce_sum(z * self.bins, axis=-1)       # out: [None, n_actions]
     center  = self.bins - tf.expand_dims(q, axis=-1)      # out: [None, n_actions, N]
     z_var   = tf.square(center) * z                       # out: [None, n_actions, N]
     z_var   = tf.reduce_sum(z_var, axis=-1)               # out: [None, n_actions]
 
     # Normalize the variance
-    a_var   = tf.reduce_sum(tf.square(z_var), axis=-1)    # out: [None]
-    a_var   = tf.expand_dims(tf.sqrt(a_var), axis=-1)     # out: [None, 1]
-    z_var   = z_var / a_var                               # out: [None, n_actions]
+    # a_var   = tf.reduce_mean(tf.square(z_var), axis=-1)   # out: [None]
+    # a_var   = tf.expand_dims(tf.sqrt(a_var), axis=-1)     # out: [None, 1]
+    # z_var   = z_var / a_var                               # out: [None, n_actions]
+    mean    = tf.reduce_mean(z_var, axis=-1, keep_dims=True)  # out: [None, 1]
+    z_var   = z_var / mean                                    # out: [None, n_actions]
 
     return z_var
 