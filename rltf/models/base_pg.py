fix Gaussian KL, working TRPO, distributions naming

rltf/models/base_pg.py
@@ -169,7 +169,7 @@ class BasePG(Model):
 
   def _act_train(self, pi, vf, name):
     action  = tf.identity(pi.sample(), name=name)
-    logp    = pi.logp(action)
+    logp    = pi.log_prob(action)
     return dict(action=action, vf=vf, logp=logp)
 
 

rltf/models/ppo.py
@@ -53,7 +53,7 @@ class PPO(BasePG):
 
     # Compute the policy gradient loss
     adv       = self.adv_norm
-    logp      = pi.logp(self.act_ph)
+    logp      = pi.log_prob(self.act_ph)
     weights   = tf.exp(logp - self.old_logp_ph)
     pg_loss_1 = weights * adv
     pg_loss_2 = tf.clip_by_value(weights, 1 - CLIP_RANGE, 1 + CLIP_RANGE) * adv

rltf/models/reinforce.py
@@ -37,7 +37,7 @@ class REINFORCE(BasePG):
 
 
   def _compute_loss(self, pi, vf, tb_name):
-    logp    = pi.logp(self.act_ph)
+    logp    = pi.log_prob(self.act_ph)
     pg_loss = - tf.reduce_mean(logp * self.adv_norm)
     vf_loss = tf.losses.mean_squared_error(self.ret_ph, vf)
     loss    = vf_loss + pg_loss

rltf/models/trpo.py
@@ -98,11 +98,11 @@ class TRPO(BasePG):
 
   def _compute_losses(self, pi, old_pi, vf):
     # Compute the KL divergence between the two policies
-    mean_kl = tf.reduce_mean(old_pi.kl(pi))
+    mean_kl = tf.reduce_mean(old_pi.kl_divergence(pi))
 
     # Compute the policy gradient maximization objective: advantage * p_new / p_old
-    pg_objective = self.adv_norm * tf.exp(pi.logp(self.act_ph) - self.old_logp_ph)
-    # pg_objective = self.adv_norm * tf.exp(pi.logp(self.act_ph) - old_pi.logp(self.act_ph))
+    pg_objective = self.adv_norm * tf.exp(pi.log_prob(self.act_ph) - self.old_logp_ph)
+    # pg_objective = self.adv_norm * tf.exp(pi.log_prob(self.act_ph) - old_pi.log_prob(self.act_ph))
     pg_objective = tf.reduce_mean(pg_objective)
 
     # Compute the policy entropy for Max-Ent learning

rltf/tf_utils/distributions.py
@@ -8,7 +8,7 @@ class ProbabilityDistribution:
   def sample(self):
     raise NotImplementedError()
 
-  def logp(self, x):
+  def log_prob(self, x):
     """Compute the log probability of x
     Args:
       x: tf.Tensor. Shape varies by class
@@ -24,7 +24,7 @@ class ProbabilityDistribution:
     """
     raise NotImplementedError()
 
-  def kl(self, other):
+  def kl_divergence(self, other):
     """Compute D_KL(self || other)
     Returns:
       tf.Tensor of shape `[None]`. The size is determined by the batch size
@@ -53,7 +53,7 @@ class CategoricalPD(ProbabilityDistribution):
     return tf.squeeze(tf.multinomial(self.logits, num_samples=1), axis=-1)
 
 
-  def logp(self, x):
+  def log_prob(self, x):
     """
     Args:
       x: tf.Tensor, shape=`[batch_size]`. The class for which to compute logp
@@ -76,7 +76,7 @@ class CategoricalPD(ProbabilityDistribution):
     return entropy
 
 
-  def kl(self, other):
+  def kl_divergence(self, other):
     assert isinstance(other, self.__class__)
     assert self.logits.shape[-1] == other.logits.shape[-1]
 
@@ -115,7 +115,7 @@ class DiagGaussianPD(ProbabilityDistribution):
     return self.mean + tf.random_normal(shape=tf.shape(self.mean)) * self.std
 
 
-  def logp(self, x):
+  def log_prob(self, x):
     """
     Args:
       x: tf.Tensor, shape=`[batch_size, self.dim]`
@@ -132,23 +132,16 @@ class DiagGaussianPD(ProbabilityDistribution):
     return 0.5 * self.dim * (np.log(2*np.pi) + 1) + tf.reduce_sum(self.logstd, axis=-1)
 
 
-  def kl(self, other):
+  def kl_divergence(self, other):
     assert isinstance(other, self.__class__)
     assert other.dim == self.dim
 
     return tf.reduce_sum( (
-                            0.5 * tf.square((self.std + (self.mean - other.mean)) / other.std)
-                            + other.logstd - self.logstd
+                            0.5 * tf.square(self.std / other.std) +
+                            0.5 * tf.square((self.mean - other.mean) / other.std) +
+                            other.logstd - self.logstd
                           ),
                           axis=-1) - 0.5 * self.dim
-    # return tf.reduce_sum( (
-    #                         0.5 * tf.square(tf.exp(self.logstd - other.std))
-    #                         + 0.5 * tf.square((self.mean - other.mean) / other.std)
-    #                         # + 0.5 * tf.square((self.mean - other.mean) / other.std)
-    #                         + other.logstd - self.logstd
-    #                       ),
-    #                       axis=-1) - 0.5 * self.dim
-
 
   @property
   def dimension(self):